265d6f95e8a6e913ab3b5c2366faa5ee3d617b61
Gia Huy Dev <huypmg.16@gmail.com>
2024-06-09 21:02:51 +0700

    Finish the export pdf files

--- client/app/src/main/java/com/dacs/vku/util/Constants.kt
+++ client/app/src/main/java/com/dacs/vku/util/Constants.kt
@@ -2,7 +2,7 @@ package com.dacs.vku.util
 
 class Constants {
     companion object {
-        const val BASE_URL = "http://192.168.1.133:3000/"
+        const val BASE_URL = "http://10.50.97.167:3000/"
         const val QUERY_PAGE_SIZE = "20"
         const val SEARCH_NEWS_TIME_DELAY = "500L"
         const val PERMISSIONS_REQUEST_WRITE_CALENDAR = 100
